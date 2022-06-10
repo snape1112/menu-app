c64fa134a0c009123a819c1dd173fbee5eaeee33
snape1112 <snape793@gmail.com>
2022-06-11 07:17:11 +0800

    update urls

--- django/proj/settings_prod.py
+++ django/proj/settings_prod.py
@@ -122,9 +122,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = BASE_DIR + "/static"
 
 CORS_ORIGIN_WHITELIST = (
 )
