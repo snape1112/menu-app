33925bcff21a0495f09607f4a3e8c4d0ddf1a539
ithunter101 <ittechhunter513@gmail.com>
2022-06-11 07:13:36 +0800

    update settings

--- django/proj/settings.py
+++ django/proj/settings.py
@@ -1,11 +1,10 @@
 import os
 
-# if 'DJANGO_SETTINGS' in os.environ:
-#     if os.environ['DJANGO_SETTINGS'] == "dev":
-#         print ("DEV SERVER")
-#         from .settings_dev import *
-# else:
-#     print ("PROD SERVER")
-#     from .settings_prod import *
+if 'DJANGO_SETTINGS' in os.environ:
+    if os.environ['DJANGO_SETTINGS'] == "dev":
+        print ("DEV SERVER")
+        from .settings_dev import *
+else:
+    print ("PROD SERVER")
+    from .settings_prod import *
 
-from .settings_dev import *
