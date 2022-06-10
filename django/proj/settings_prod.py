90e66efcbb63517e243e0f0a1ae96507194fcb58
snape1112 <snape793@gmail.com>
2022-06-10 13:39:54 +0800

    change static

--- django/proj/settings_prod.py
+++ django/proj/settings_prod.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['35.224.227.148', '35.223.175.63']
+ALLOWED_HOSTS = ['35.224.227.148', '35.225.48.123']
 
 
 # Application definition
