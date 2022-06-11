da0e6be75603ad062ba100356c6f8403bff015e9
ithunter101 <ittechhunter513@gmail.com>
2022-06-11 13:30:16 +0800

    update settings

--- django/proj/settings_prod.py
+++ django/proj/settings_prod.py
@@ -123,7 +123,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_ROOT = "/static/"
+
+STATIC_URL = "https://storage.googleapis.com/display-menu/"
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'media'),
+]
 
 CORS_ORIGIN_WHITELIST = (
 )
