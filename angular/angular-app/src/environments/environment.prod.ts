4b19d5f5a236eb74d930f001ffe7268c621a8d05
snape1112 <snape793@gmail.com>
2022-06-11 21:32:20 +0800

    update ip

--- angular/angular-app/src/environments/environment.prod.ts
+++ angular/angular-app/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://35.225.48.123/api',
-  serverUrl:  'http://35.225.48.123',
+  apiUrl: 'http://34.135.15.3/api',
+  serverUrl:  'http://34.135.15.3',
 };
