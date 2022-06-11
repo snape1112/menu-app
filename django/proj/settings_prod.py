11a0f0e074f81e6e3ddfce9c0e40648a6e6fae86
ithunter101 <ittechhunter513@gmail.com>
2022-06-11 23:55:16 +0800

    update

--- django/proj/settings_prod.py
+++ django/proj/settings_prod.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_ROOT = "https://storage.googleapis.com/display-menu/static"
+# STATIC_ROOT = "https://storage.googleapis.com/display-menu/static"
 STATIC_URL = '/static/'
 
 # DEFAULT_FILE_STORAGE = 'storages.backends.gcloud.GoogleCloudStorage'
